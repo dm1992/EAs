c11c237bd6bc3e0f450cb9b539c815799cd3b70c
Davor Markovic <davor.markovic@avera.si>
2023-11-20 14:10:23 +0100

    Removed unused manager.

--- Crypto/CryptoBot/DataAnalyzer/Managers/TradingStrategyBacktestManager.cs
+++ Crypto/CryptoBot/DataAnalyzer/Managers/TradingStrategyBacktestManager.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace DataAnalyzer.Managers
 {
-    public class MarketBacktestManager
+    public class TradingStrategyBacktestManager
     {
     }
 }
--- Crypto/CryptoBot/DataAnalyzer/Program.cs
+++ Crypto/CryptoBot/DataAnalyzer/Program.cs
@@ -1,6 +1,4 @@
 ﻿using Common;
-using MarketAnalyzer.Managers;
-using MarketAnalyzer.Models;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -14,60 +12,7 @@ namespace DataAnalyzer
     {
         private static void Main(string[] args)
         {
-            try
-            {
-                List<DrawingLookupType> drawingLookupTypes = ParseDrawingLookupTypes(args);
-                if (drawingLookupTypes.IsNullOrEmpty())
-                    return;
-
-                LotteryManager lotteryManager = new LotteryManager();
-                string sourceFilePath = "C:\\Users\\Davor\\Delo\\Projekti\\Lokalno\\EAs\\Crypto\\CryptoBot\\DataAnalyzer\\Data\\results_ej.csv";
-                string destinationFilePath = "C:\\Users\\Davor\\Delo\\Projekti\\Lokalno\\EAs\\Crypto\\CryptoBot\\DataAnalyzer\\Data\\";
-
-                if (!lotteryManager.ParseDrawings(sourceFilePath))
-                {
-                    Console.WriteLine($"Failed to parse eurojackpot drawings from source '{sourceFilePath}'.");
-                    return;
-                }
-
-                foreach (DrawingLookupType dlt in drawingLookupTypes)
-                {
-                    Console.WriteLine($"Drawing number frequency on {dlt}.");
-
-                    foreach (var drawingNumberFrequency in lotteryManager.GetDrawingNumberFrequencies(dlt).OrderBy(x => x.Value ?? 0))
-                    {
-                        Helpers.WriteToFile(drawingNumberFrequency.Dump(), Path.Combine(destinationFilePath, $"{dlt}_frequency.txt"));
-                    }
-                }
-
-                Console.WriteLine("Eurojackpot analysis results written.");
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine($"Fatal error ocurred: {e.Message}");
-            }
-            finally
-            {
-                Console.ReadLine();
-            }
-        }
-
-        private static List<DrawingLookupType> ParseDrawingLookupTypes(string [] args)
-        {
-            List<DrawingLookupType> drawingLookupTypes = new List<DrawingLookupType>();
-
-            if (args.Length < 1)
-            {
-                Console.WriteLine("Pass arguments!");
-                return drawingLookupTypes;
-            }
-
-            foreach (var arg in args)
-            {
-                drawingLookupTypes.Add((DrawingLookupType)Enum.Parse(typeof(DrawingLookupType),arg));
-            }
-
-            return drawingLookupTypes;
+            
         }
     }
 }
